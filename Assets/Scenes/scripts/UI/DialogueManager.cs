b9d35ce1c07434b03171760b6516793405a34371
Maren Skårestuen Grindal <marengrindal@gmail.com>
2022-10-17 13:04:01 +0200

    [#33] Add page overflow to dialogue system

--- Assets/Scenes/scripts/UI/DialogueManager.cs
+++ Assets/Scenes/scripts/UI/DialogueManager.cs
@@ -12,12 +12,16 @@ public class DialogueManager : MonoBehaviour
     public string[] sentences;
     int index;
     bool isRunning;
+    int totalPages;
+    int currentPage;
 
     // Start is called before the first frame update
     void Start()
     {
         index = -1;
         isRunning = false;
+        totalPages = 0;
+        currentPage = 0;
         ToggleTextBox();
     }
 
@@ -28,9 +32,23 @@ public class DialogueManager : MonoBehaviour
         {
             ToggleTextBox();
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && textBox.gameObject.activeSelf)
         {
-            // if coroutine is running, the current sentence needs to be completely written out
+            totalPages = text.textInfo.pageCount;
+            // change page if the current text contains multiple pages
+            if (totalPages > 1 && (currentPage < totalPages))
+            {
+                ChangePage();
+            } else
+            // change sentence and reset page variables
+            {
+                currentPage = 1;
+                text.pageToDisplay = 1;
+                ChangeSentence();
+            }
+           
+            //ChangePage();
+            /*/ if coroutine is running, the current sentence needs to be completely written out
             if (isRunning)
             {
                 isRunning = false;
@@ -40,8 +58,15 @@ public class DialogueManager : MonoBehaviour
             else
             {
                 ChangeSentence();
-            }
+            }*/
         }
+    }
+
+    // OnDisable is called when the game object becomes disabled
+    void OnDisable()
+    {
+        text.text = "";
+        index = -1;
     }
 
     /// <summary>
@@ -50,13 +75,6 @@ public class DialogueManager : MonoBehaviour
     void ToggleTextBox()
     {
         textBox.gameObject.SetActive(!textBox.gameObject.activeSelf);
-    }
-
-    // OnDisable is called when the game object becomes disabled
-    void OnDisable()
-    {
-        text.text = "";
-        index = -1;
     }
 
     /// <summary>
@@ -68,22 +86,45 @@ public class DialogueManager : MonoBehaviour
         {
             index = -1;
         }
-        text.text = "";
+        index++;
+        text.text = sentences[index];
+        
+        /*text.text = "";
         isRunning = true;
         index++;
-        StartCoroutine(IterateSentence());
+        StartCoroutine(IterateSentence());*/
+    }
+
+    /// <summary>
+    /// Change the page to be displayed from the current text.
+    /// </summary>
+    void ChangePage()
+    {
+        text.pageToDisplay++;
+        currentPage++;
     }
 
     /// <summary>
     /// Write every letter in a sentence with a delay.
     /// </summary>
-    IEnumerator IterateSentence()
+    /*IEnumerator IterateSentence()
     {
+        int totalPages = text.textInfo.pageCount;
+        int currentPage = 1;
+
         foreach (char c in sentences[index])
         {
+            // check if the page has changed
+            if (text.isTextOverflowing && (currentPage < totalPages))
+            {
+                text.pageToDisplay++;
+                currentPage++;
+            }
             text.text += c;
+            //print(c);
+           
             yield return new WaitForSeconds(0.05f);
         }
         isRunning = false;
-    }
+    }*/
 }
