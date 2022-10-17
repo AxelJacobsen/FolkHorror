4243f52785b3265dbb273dfbb5db224204ac99aa
Maren Skårestuen Grindal <marengrindal@gmail.com>
2022-10-17 15:07:49 +0200

    [#33] Make typing of text work with pages

--- Assets/Scenes/scripts/UI/DialogueManager.cs
+++ Assets/Scenes/scripts/UI/DialogueManager.cs
@@ -10,16 +10,18 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI text;
 
     public string[] sentences;
+    string currentText;
     int index;
     bool isRunning;
 
     // Start is called before the first frame update
     void Start()
     {
-        ToggleTextBox();
-        index = -1;
         text.text = "";
+        currentText = "";
+        index = -1;
         isRunning = false;
+        ToggleTextBox();
     }
 
     // Update is called once per frame
@@ -33,33 +35,20 @@ public class DialogueManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && textBox.gameObject.activeSelf)
         {
             // check for overflow, then we know if there are still pages left
-            if (text.isTextOverflowing)
+            if (isRunning)
+            {
+                isRunning = false;
+                StopAllCoroutines();
+                text.text = currentText;
+            }
+            else if (text.isTextOverflowing)
             {
                 ChangePage();
             } else
             {
                 ChangeSentence();
             }
-
-            /*/ if coroutine is running, the current sentence needs to be completely written out
-            if (isRunning)
-            {
-                isRunning = false;
-                StopAllCoroutines();
-                text.text = sentences[index];
-            }
-            else
-            {
-                ChangeSentence();
-            }*/
         }
-    }
-
-    // OnDisable is called when the game object becomes disabled
-    void OnDisable()
-    {
-        text.text = "";
-        index = -1;
     }
 
     /// <summary>
@@ -80,14 +69,16 @@ public class DialogueManager : MonoBehaviour
             index = -1;
         }
 
+        // make text ready for typing
+        text.text = "";
+
         // set new sentence
         index++;
-        text.text = sentences[index];
+        currentText = sentences[index];
 
-        /*text.text = "";
+        // start typing effect
         isRunning = true;
-        index++;
-        StartCoroutine(IterateSentence());*/
+        StartCoroutine(IterateSentence(currentText));
     }
 
     /// <summary>
@@ -95,22 +86,28 @@ public class DialogueManager : MonoBehaviour
     /// </summary>
     void ChangePage()
     {
-        // remove current text from text game object
+        // remove used page from text
         text.text = text.text.Substring(text.firstOverflowCharacterIndex);
+        currentText = text.text;
+
+        text.text = "";
+
+        // start typing current page
+        isRunning = true;
+        StartCoroutine(IterateSentence(currentText));
     }
 
     /// <summary>
     /// Write every letter in a sentence with a delay.
-    /*// </summary>
-    IEnumerator IterateSentence()
+    /// </summary>
+    IEnumerator IterateSentence(string page)
     {
-        foreach (char c in sentences[index])
+        foreach (char c in page)
         {
             text.text += c;
-            //print(c);
            
             yield return new WaitForSeconds(0.05f);
         }
         isRunning = false;
-    }*/
+    }
 }
