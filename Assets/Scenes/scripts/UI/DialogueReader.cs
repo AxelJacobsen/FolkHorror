093d1888cf9a30280301cbd524ae295fd33c1645
Maren Skårestuen Grindal <marengrindal@gmail.com>
2022-10-25 14:18:23 +0200

    [#63] Modify XML reader function

--- Assets/Scenes/scripts/UI/DialogueReader.cs
+++ Assets/Scenes/scripts/UI/DialogueReader.cs
@@ -1,33 +1,29 @@
 using System;
 using System.IO;
 using System.Xml.Serialization;
+using System.Xml;
 using UnityEngine;
 
 /// <summary>
 /// Class <c>DialogueReader</c> reads dialogue from XML file.
 /// </summary>
-public class DialogueReader : MonoBehaviour
+public class DialogueReader
 {
     /// <summary>
-    /// Read an xml file.
+    /// Reads XML files into an object.
+    /// Code taken from: https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/serialization/how-to-read-object-data-from-an-xml-file
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="path"></param>
-    /// <returns></returns>
-    public static T ImportXml<T>(string path)
+    /// <typeparam name="T">Object the XML content is added to</typeparam>
+    /// <param name="path">Path of the XML file</param>
+    /// <returns>Object with values</returns>
+    public static T ReadXML<T>(string path)
     {
-        try
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(T));
-            using (var stream = new FileStream(path, FileMode.Open))
-            {
-                return (T)serializer.Deserialize(stream);
-            }
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("Exception importing xml file: " + e);
-            return default;
-        }
+        System.Xml.Serialization.XmlSerializer reader = 
+            new System.Xml.Serialization.XmlSerializer(typeof(T));
+        System.IO.StreamReader file = new System.IO.StreamReader(path);
+        T obj = (T)reader.Deserialize(file);
+        file.Close();
+
+        return obj;
     }
 }
