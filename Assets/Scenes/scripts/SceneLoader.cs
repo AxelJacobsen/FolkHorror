98951292c034b3373e03ba1373fb9eb49c2c6e06
Lars LR <larslru@stud.ntnu.no>
2022-10-26 20:09:06 +0200

    [#65] Fix unloading of scenes not working. Remove redundant code from SceneLoader.

--- Assets/Scenes/scripts/SceneLoader.cs
+++ Assets/Scenes/scripts/SceneLoader.cs
@@ -7,8 +7,6 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public  string StartSceneName;
-    private string nameOfSceneToLoad;
-    private string nameOfCurrentScene;
 
     void Start() {
         ChangeScene(StartSceneName);
@@ -18,26 +16,16 @@ public class SceneLoader : MonoBehaviour
     /// Change the scene.
     /// </summary>
     /// <param name="sceneName">The name of the scene to change to.</param>
-    void ChangeScene(string sceneName) {
-        nameOfCurrentScene = sceneName;
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-    }
-
-    // Set callback for loading new map scene, start unloading
-    public void LoadNewMapScene(string nameOfSceneToLoad)
-    {
-        SceneManager.sceneUnloaded += MapSceneUnloadFinished;
-        this.nameOfSceneToLoad = nameOfSceneToLoad;
-        SceneManager.UnloadSceneAsync(nameOfCurrentScene);
-    }
-
-    // Remove callback, call load for new map scene
-    private void MapSceneUnloadFinished(Scene unloadedScene)
-    {
-        SceneManager.sceneUnloaded -= MapSceneUnloadFinished;
-        if (!SceneManager.GetSceneByName(nameOfSceneToLoad).isLoaded)
+    public void ChangeScene(string sceneName) {
+        // Unload all other scenes than "NeverUnload"
+        for (int i = 0; i < SceneManager.sceneCount; i++)
         {
-            ChangeScene(nameOfSceneToLoad);
+            Scene it = SceneManager.GetSceneAt(i);
+            if (it.name != "NeverUnload")
+                SceneManager.UnloadSceneAsync(it);
         }
+
+        // Load the scene
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
     }
 }
