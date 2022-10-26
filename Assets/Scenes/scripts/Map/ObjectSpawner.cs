56767b8c2900eb0d26b75296811fd6e70c5e749e
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-10-26 22:09:19 +0200

    Map generation mostly completed, added reading from file and sample round room, not done

--- Assets/Scenes/scripts/Map/ObjectSpawner.cs
+++ Assets/Scenes/scripts/Map/ObjectSpawner.cs
@@ -7,14 +7,27 @@ using UnityEngine;
 /// </summary>
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject Prototype;
+    public GameObject Tree;
+    public GameObject Bush;
+    public GameObject Chest;
+    public GameObject Exit;
+    public GameObject Grass;
+    public GameObject Rocks;
 
     /// <summary>
     /// Spawns the prototype at a given position.
     /// </summary>
     /// <param name="position">The given position.</param>
-    public void SpawnObject(Vector3 position) {
-        GameObject newTree = Instantiate(Prototype, position, Quaternion.Euler(45, 0, 0) );
+    public void SpawnObject(Vector3 position, int renderIndex) {
+        if (renderIndex == -1) { return; }
+        List<GameObject> renderList = new List<GameObject>() { Tree, Bush, Chest, Exit, Grass, Rocks };
+        GameObject renderObject = renderList[renderIndex];
+        SpriteRenderer sprite = renderObject.GetComponent<SpriteRenderer>();
+        position.y += ((sprite.bounds.size.y) / 2 )* Mathf.Sin(45)*(0.83f);
+        position.z += ((sprite.bounds.size.y) / 2 )* Mathf.Sin(45)*(0.83f);
+        position.x += (sprite.bounds.size.x / 4 );
+        GameObject newTree = Instantiate(renderObject, position, Quaternion.Euler(45, 0, 0) );
         newTree.SetActive(true);
+        
     }
 }
