7214ee4f0cad0e7146cf7294b2c42520705f6763
Maren <marengrindal@gmail.com>
2022-12-12 19:00:13 +0100

    [#142] Fix relationship between quests and dialogue

--- Assets/Scripts/Quest/LocationQuest.cs
+++ Assets/Scripts/Quest/LocationQuest.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.InputSystem;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class LocationQuest : Quest
 {
-    public List<Dialogue> startDialogue;
-    public List<Dialogue> endDialogue;
+    public List<Dialogue> startDialogue = new();
+    public List<Dialogue> endDialogue = new();
 }
